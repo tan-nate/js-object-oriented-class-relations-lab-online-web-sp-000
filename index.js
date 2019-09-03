160e0ffe08d62170a42a6fe5f7a05db915c0c8e5
Nate Tan <nktan93@gmail.com>
2019-09-03 15:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let store = { drivers: [] };
- 
+
 let driverId = 0;
- 
+
 class Driver {
     constructor(name) {
         this.id = ++driverId;
         this.name = name;
- 
+
         // insert in the user to the store
         store.users.push(this);
     }
 }
- 
+
 let itemId = 0;
- 
+
 class Item {
     constructor(name, price, user) {
         this.id = ++itemId;
@@ -22,7 +22,7 @@ class Item {
         if (user) {
             this.userId = user.id;
         }
- 
+
         // insert in the item to the store
         store.items.push(this);
     }
