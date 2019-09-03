626066acc0444848f7271a2b3cf1ebc5f1ad47a7
Nate Tan <nktan93@gmail.com>
2019-09-03 16:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,4 +42,11 @@ class Trip {
             }.bind(this)
         );
     }
+    driver() {
+        return store.drivers.find(
+            function(driver) {
+                return driver.id === this.driverId;
+            }.bind(this)
+        );
+    }
 }
