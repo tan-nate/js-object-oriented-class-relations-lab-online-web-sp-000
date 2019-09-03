964f7a4b59a36181bcde8092753d657bacc75bc2
Nate Tan <nktan93@gmail.com>
2019-09-03 15:54:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,32 @@
+let store = { drivers: [] };
+ 
+let driverId = 0;
+ 
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+ 
+        // insert in the user to the store
+        store.users.push(this);
+    }
+}
+ 
+let itemId = 0;
+ 
+class Item {
+    constructor(name, price, user) {
+        this.id = ++itemId;
+        this.name = name;
+        this.price = price;
+        if (user) {
+            this.userId = user.id;
+        }
+ 
+        // insert in the item to the store
+        store.items.push(this);
+    }
+    setUser(user) {
+        this.userId = user.id;
+    }
+}
